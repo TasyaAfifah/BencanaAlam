6522331d2fc58a3eb1fa0fdd5f4b053da61961d1
BayuKharismulloh <bayukh15@gmail.com>
2021-09-18 21:46:44 +0700

    update bayu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InformasiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/', function () {
 Route::get('/Coba', function () {
     return view('Coba');
 });
+
+Route::get('/informasi', [InformasiController::class, 'index']);
