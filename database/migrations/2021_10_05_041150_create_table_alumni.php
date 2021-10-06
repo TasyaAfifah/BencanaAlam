3617179e18ccda59c04df5c6cf20caa6034e8d6e
TasyaAfifah <tasyaafifah1234@gmail.com>
2021-10-06 12:39:25 +0700

    nambah

--- database/migrations/2021_10_05_041150_create_table_alumni.php
+++ database/migrations/2021_10_05_041150_create_table_alumni.php
@@ -16,7 +16,7 @@ class CreateTableAlumni extends Migration
         Schema::create('table_alumni', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("nama",50);
+         
         });
     }
 
