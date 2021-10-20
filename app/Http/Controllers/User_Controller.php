28a7d4e990f5fd13a847cb9ee2ada2b55a5f895e
TasyaAfifah <tasyaafifah1234@gmail.com>
2021-10-20 20:20:48 +0700

    Update Controller dan Views

--- app/Http/Controllers/User_Controller.php
+++ app/Http/Controllers/User_Controller.php
@@ -2,9 +2,85 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\UserRole;
 use Illuminate\Http\Request;
 
 class User_Controller extends Controller
 {
-    //
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserRole  $userRole
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserRole $userRole)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserRole  $userRole
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserRole $userRole)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserRole  $userRole
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserRole $userRole)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserRole  $userRole
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserRole $userRole)
+    {
+        //
+    }
 }
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -117,10 +117,10 @@
           </div>
 
           <!-- Slide 2 -->
-          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-2.jpg');">
+          <div class="carousel-item" style="background-image: url('assets/img/slide/TanahLongsor.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
-                <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
+                <h2 class="animate__animated animate__fadeInDown"> Judul </h2>
                 <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
                 <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
               </div>
@@ -128,7 +128,7 @@
           </div>
 
           <!-- Slide 3 -->
-          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-3.jpg');">
+          <div class="carousel-item" style="background-image: url('assets/img/slide/Banjir.jpeg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
@@ -167,13 +167,13 @@
 
             <div class="section-title">
               <h2>About Us</h2>
-              <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.</p>
+              <p>Disini kami akan membantu evakuasi korban bencana alam , memberi bantuan , dan berusaha semaksimal mungkin agar korban bencana alam mendaptkan faslisitas yang layak </p>
             </div>
 
             <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
               <div class="icon"><i class="bx bx-fingerprint"></i></div>
-              <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <h4 class="title"><a href="">Call Center</a></h4>
+              <p class="description">1000 777</p>
             </div>
 
             <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
